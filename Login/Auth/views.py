571391cc0085eac3fb44fd1ca5a0fcf241224689
KittyLee <katherine@codeforprogress.org>
2014-07-09 15:31:42 -0400

    added imports in views

--- Login/Auth/views.py
+++ Login/Auth/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import Permission, User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
